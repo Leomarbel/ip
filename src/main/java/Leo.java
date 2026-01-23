31fb6300821b3f5ff6e2b6b2ce882ebf21a8bab8
Marnige <dragonmasteroffire@gmail.com>
2026-01-24 02:41:31 +0800

    Add Level-4

--- src/main/java/Leo.java
+++ src/main/java/Leo.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -34,11 +35,11 @@ public class Leo {
                     break;
 
                 case "add":
-                    handleAdd(scanner, list);
+                    HandleAdd(scanner, list);
                     break;
 
                 case "list":
-                    printList(list);
+                    PrintList(list);
                     break;
 
                 default:
@@ -55,7 +56,7 @@ public class Leo {
         System.out.println(sep);
     }
 
-    private static void handleAdd(Scanner scanner, ArrayList<Task> list) {
+    private static void HandleAdd(Scanner scanner, ArrayList<Task> list) {
         LeoReply("Adding to list now (type 'stop' to finish, 'list' to view)");
 
         boolean stopListing = false;
@@ -67,45 +68,71 @@ public class Leo {
 
             switch (command) {
                 case "list":
-                    printList((list));
+                    PrintList((list));
                     break;
+
                 case "stop":
                     stopListing = true;
                     break;
+
                 case "mark":
                     int index = Integer.parseInt(parts[1]) - 1;
-                    markTask(index, list);
+                    MarkTask(index, list);
                     break;
+
                 case "unmark":
                     int index1 = Integer.parseInt(parts[1]) - 1;
-                    unmarkTask(index1, list);
+                    UnmarkTask(index1, list);
                     break;
+
+                case "todo":
+                    TodoTask(parts[1], list);
+                    break;
+
+                case "deadline":
+                    String[] deadlineParts = parts[1].split("/by", 2);
+
+                    if (deadlineParts.length < 2) {
+                        LeoReply("Error!!! The description of a deadline must have /by.");
+                        return;
+                    }
+
+                    DeadlineTask(deadlineParts[0].trim(),
+                                 list,
+                                 deadlineParts[1].trim());
+                    break;
+
+                case "event":
+                    String[] eventParts = parts[1].split("/from", 2);
+
+                    if (eventParts.length < 2) {
+                        LeoReply("Error!!! The description of an event must have /from.");
+                        return;
+                    }
+
+                    String[] eventDuration = eventParts[1].split("/to", 2);
+                    if (eventDuration.length < 2) {
+                        LeoReply("Error!!! The description of an event must have /to.");
+                        return;
+                    }
+                    EventTask(parts[0],
+                              list,
+                              eventDuration[0],
+                              eventDuration[1]);
+                    break;
+
                 default:
                     list.add(new Task(listing, false));
                     LeoReply("Added: " + listing);
 
             }
 
-            /*if (listing.equalsIgnoreCase("list")) {
-                printList(list);
-            } else if (listing.equalsIgnoreCase("stop")) {
-                stopListing = true;
-            } else if (listing.startsWith("mark ")) {
-                int index = Integer.parseInt(listing.substring(5)) - 1;
-                markTask(index, list);
-            } else if (listing.startsWith("unmark ")) {
-                int index = Integer.parseInt(listing.substring(7)) - 1;
-                unmarkTask(index, list);
-            } else {
-                list.add(new Task(listing, false));
-                LeoReply("Added: " + listing);
-            } */
         }
     }
 
-    private static void printList(ArrayList<Task> list) {
+    private static void PrintList(ArrayList<Task> list) {
         if (list.isEmpty()) {
-            LeoReply("(list is empty)");
+            LeoReply("(List is empty)");
             return;
         }
         StringBuilder listText = new StringBuilder();
@@ -121,12 +148,11 @@ public class Leo {
 
     public static void LeoReply(String text) {
         PrintSep();
-
         System.out.println("Leo: \n" + text);
         PrintSep();
     }
 
-
+    // region Classes
     public static class Task {
         private boolean hasMarked;
         private final String task;
@@ -136,6 +162,14 @@ public class Leo {
             this.hasMarked = hasMarked;
         }
 
+        public void mark(){
+            this.hasMarked = true;
+        }
+
+        public void unmark() {
+            this.hasMarked = false;
+        }
+
         @Override
         public String toString() {
             String mark = hasMarked? "X" : " ";
@@ -143,26 +177,88 @@ public class Leo {
         }
     }
 
-    private static void markTask(int index, ArrayList<Task> list) {
+    public static class Todo extends Task {
+
+        public Todo(String task, boolean hasMarked) {
+            super(task, hasMarked);
+        }
+
+        @Override
+        public String toString() {
+            String mark = super.hasMarked? "X" : " ";
+            return "[T]" + "[" + mark + "] " + super.task;
+        }
+    }
+
+    public static class Deadline extends Task {
+        private final String deadline;
+        public Deadline(String task, boolean hasMarked, String deadline) {
+            super(task, hasMarked);
+            this.deadline = deadline;
+        }
+
+        @Override
+        public String toString() {
+            String mark = super.hasMarked? "X" : " ";
+            return "[D]" + "[" + mark + "] " + super.task + " [Due: " + deadline + "]";
+        }
+    }
+
+    public static class Event extends Task {
+        private String start;
+        private String end;
+        public Event(String task, boolean hasMarked, String start, String end) {
+            super(task, hasMarked);
+            this.start = start;
+            this.end = end;
+        }
+
+        @Override
+        public String toString() {
+            String mark = super.hasMarked? "X" : " ";
+            return "[E]" + "[" + mark + "] " + super.task + " [" + start + " --- " + end + "]";
+        }
+    }
+    // endregion
+
+    private static void MarkTask(int index, ArrayList<Task> list) {
         if (index > list.size() - 1 || index < 0) {
-            LeoReply("Error, Index outside of list bounds :(");
+            LeoReply("Error!!! Index outside of list bounds :(");
             return;
         }
 
         Task t = list.get(index);
-        t.hasMarked = true;
+        t.mark();
         LeoReply(t.toString());
     }
 
-    private static void unmarkTask(int index, ArrayList<Task> list) {
+    private static void UnmarkTask(int index, ArrayList<Task> list) {
         if (index > list.size() - 1 || index < 0) {
-            LeoReply("Error, Index outside of list bounds :(");
+            LeoReply("Error!!! Index outside of list bounds :(");
             return;
         }
 
         Task t = list.get(index);
-        t.hasMarked = false;
+        t.unmark();
         LeoReply(t.toString());
+    }
+
+    private static void TodoTask(String task, ArrayList<Task> list) {
+        Todo t = new Todo(task, false);
+        list.add(t);
+        LeoReply(t.toString() + "\n Current Tasks: " + list.size() );
+    }
+
+    private static void DeadlineTask(String task, ArrayList<Task> list, String deadline) {
+        Deadline t = new Deadline(task, false, deadline);
+        list.add(t);
+        LeoReply(t.toString() + "\n Current Tasks: " + list.size() );
+    }
+
+    private static void EventTask(String task, ArrayList<Task> list, String start, String end) {
+            Event t = new Event(task, false, start, end);
+        list.add(t);
+        LeoReply(t.toString() + "\n Current Tasks: " + list.size() );
     }
 
 
