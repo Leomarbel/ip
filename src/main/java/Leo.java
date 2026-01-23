a08717b6ec7ee2ac120ba8c750f9133149386d0e
Marnige <dragonmasteroffire@gmail.com>
2026-01-23 15:39:36 +0800

    Added User Input

--- src/main/java/Leo.java
+++ src/main/java/Leo.java
@@ -1,18 +1,35 @@
+import java.util.Scanner;
+
 public class Leo {
     public static void main(String[] args) {
-        String logo = "██╗     ███████╗ ██████╗\n" +
-                      "██║     ██╔════╝██╔═══██╗\n" +
-                      "██║     █████╗  ██║   ██║\n" +
-                      "██║     ██╔══╝  ██║   ██║\n" +
-                      "███████╗███████╗╚██████╔╝\n" +
-                      "╚══════╝╚══════╝ ╚═════╝ ";
+        boolean isExiting = false;
+        Scanner scanner = new Scanner(System.in);
+        String logo = """
+            ██╗     ███████╗ ██████╗
+            ██║     ██╔════╝██╔═══██╗
+            ██║     █████╗  ██║   ██║
+            ██║     ██╔══╝  ██║   ██║
+            ███████╗███████╗╚██████╔╝
+            ╚══════╝╚══════╝ ╚═════╝
+            """;
 
 
         PrintSep();
         System.out.println("Hello! I'm \n" + logo);
         System.out.println("What can I do for you? \n");
         PrintSep();
-        System.out.println("Bye. Hope to see you again soon!\n");
+
+        while (!isExiting) {
+
+            String input = scanner.nextLine();
+
+            System.out.println("You: " + input);
+            if (input.equalsIgnoreCase("bye")
+            ) isExiting = true;
+        }
+
+        PrintSep();
+        System.out.println("Bye. Hope to see you again soon!");
     }
 
     public static void PrintSep() {
